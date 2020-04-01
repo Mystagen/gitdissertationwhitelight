4f8239456e343c7c7eea9f7f760e2df342a65b13
Michael Fraser <mj.fraser99@icloud.com>
2020-04-01 15:47:10 +0100

    Deleted remaining obsolete files

--- routes/addAccessRights.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('addAccessRights', {tabtitle: 'Documentation Software', pagetitle: 'Access Rights', search: 'false', topnav: 'none', navSelected: 'accessRights', subNavSelected: 'none'});
-});
-
-module.exports = router;
--- routes/userManagement.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('userManagement', {tabtitle: 'Documentation Software', pagetitle: 'User Management', topnav: 'none', search: 'false', navSelected: 'userManagement', subNavSelected: 'none'});
-});
-
-module.exports = router;
